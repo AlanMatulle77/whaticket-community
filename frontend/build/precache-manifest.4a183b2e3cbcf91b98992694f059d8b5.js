1fcdce475a9a04c7cd3ad764e4de3cb4d9d19c5a
AlanMatulle77 <105324394+AlanMatulle77@users.noreply.github.com>
2023-11-04 19:27:34 -0300

    Add files via upload

---
+++ frontend/build/precache-manifest.4a183b2e3cbcf91b98992694f059d8b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b9e9f135b43c68b68001a2aeef785a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24d784b36ebf2ab3055b",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "24d784b36ebf2ab3055b",
+    "url": "/static/js/2.eaa477af.chunk.js"
+  },
+  {
+    "revision": "093bf3f86145069d5ad0de50dd236547",
+    "url": "/static/js/2.eaa477af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e686aacf6c2677b31dad",
+    "url": "/static/js/3.9f3f28c1.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.9f3f28c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533873993d325fd3f789",
+    "url": "/static/js/main.29fc2c83.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.29fc2c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26ff8e6b090894d44b5",
+    "url": "/static/js/runtime-main.5e5fe3bb.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
